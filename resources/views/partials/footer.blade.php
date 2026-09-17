3de1305fd8fbb68f93e8618764e1816448cc42a6
morenoecha <morenoecha28@gmail.com>
2026-09-17 13:22:26 +0700

    hsil merge user dan admin

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -21,7 +21,7 @@
         @if(auth()->user()?->role === 'user')
             <span>Senin - Jumat : 07:30 - 16:30</span>
         @else
-            <span>Senin - Jumat: 07.00 - 17.30 WIB</span>
+            <span>Senin - Jumat: 07.30 - 16.30 WIB</span>
         @endif
         <span>Sabtu - Minggu : Libur</span>
 
@@ -36,7 +36,7 @@
             @if(auth()->user()?->role === 'user')
                 Jl. Prof. DR. Supomo No. 23,
             @else
-                Jl. Prof. DR. Supomo No.93,
+                Jl. Prof. DR. Supomo No.23,
             @endif
             Sriwedari, Kec. Laweyan,
         </span>
